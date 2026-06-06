1cd818048346b593dd23dc6d811c956c067f39da
josemiliriano <josemiliriano83@gmail.com>
2026-06-06 10:32:05 -0400

    Inicio

---
+++ ProyectoColmadoAsync/ApiColmadoAsync/MyDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiColmadoAsync
+{
+    public class MyDataContext:DbContext
+    {
+        public MyDataContext(DbContextOptions<MyDataContext> options) : base(options)
+        {
+
+        }
+    }
+}
